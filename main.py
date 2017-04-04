e49bbef19c351916601a9c8dcc0c472e7bf4da89
Rok <rok.povsic@gmail.com>
2017-04-04 19:54:30 +0200

    Snov predavanja 22.

--- main.py
+++ main.py
@@ -2,7 +2,8 @@
 import os
 import jinja2
 import webapp2
-
+import json
+from google.appengine.api import urlfetch
 
 template_dir = os.path.join(os.path.dirname(__file__), "templates")
 jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir), autoescape=False)
@@ -29,7 +30,19 @@ class BaseHandler(webapp2.RequestHandler):
 
 class MainHandler(BaseHandler):
     def get(self):
-        return self.render_template("hello.html")
+        #data = open("uporabniki.json").read()
+        url = "http://api.openweathermap.org/data/2.5/weather?q=Ljubljana&appid=9dfaa34265bbac3dca2eecb1f9222db9"
+        data = urlfetch.fetch(url).content
+
+        json_podatki = json.loads(data)
+
+        temp_kelvin = float(json_podatki["main"]["temp"])
+        temp_celzij = temp_kelvin - 273.64
+
+        spr = {
+            "json_podatki": json_podatki
+        }
+        return self.render_template("hello.html", spr)
 
 app = webapp2.WSGIApplication([
     webapp2.Route('/', MainHandler),
